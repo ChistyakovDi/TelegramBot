13b220d0df83cf545d7fcec04152ca143c18e9fb
ChistyakovDi <111275407+ChistyakovDi@users.noreply.github.com>
2023-03-15 23:26:15 +0300

    change MainService(add message handling from chat)

--- node/src/main/java/com/chistyakov/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/com/chistyakov/service/impl/ConsumerServiceImpl.java
@@ -2,11 +2,9 @@ package com.chistyakov.service.impl;
 
 import com.chistyakov.service.ConsumerService;
 import com.chistyakov.service.MainService;
-import com.chistyakov.service.ProducerService;
 import lombok.extern.log4j.Log4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import static com.chistyakov.model.RabbitQueue.*;
@@ -15,11 +13,11 @@ import static com.chistyakov.model.RabbitQueue.*;
 @Log4j
 public class ConsumerServiceImpl implements ConsumerService {
 
+    private final MainService mainService;
+
     public ConsumerServiceImpl(MainService mainService) {
         this.mainService = mainService;
     }
-
-    private final MainService mainService;
 
     @Override
     @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
@@ -32,11 +30,13 @@ public class ConsumerServiceImpl implements ConsumerService {
     @RabbitListener(queues = DOC_MESSAGE_UPDATE)
     public void consumeDocMessageUpdates(Update update) {
         log.debug("NODE: Doc message is received");
+        mainService.processDocMessage(update);
     }
 
     @Override
     @RabbitListener(queues = PHOTO_MESSAGE_UPDATE)
     public void consumePhotoMessageUpdates(Update update) {
         log.debug("NODE: Photo message is received");
+        mainService.processPhotoMessage(update);
     }
 }
